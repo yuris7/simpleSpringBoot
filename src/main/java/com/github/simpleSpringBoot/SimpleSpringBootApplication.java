97977f3b36c839ff113eb254ca7848f9f0db94ee
Yuriy Voina <yuriy.voina01@office.ctco.lv>
2018-08-14 20:32:59 +0300

    addedinitfiles

---
+++ src/main/java/com/github/simpleSpringBoot/SimpleSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.github.simpleSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootApplication.class, args);
+	}
+}
